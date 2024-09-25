21d5b4ca97cfe0075fec920457d932a32e9c9f1b
Rifat-BrainStation23 <rifat.arefin@brainstation-23.com>
2024-09-25 18:53:02 +0600

    Create  route

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,18 +1,22 @@
 class BlogPostsController < ApplicationController
+  # Get all the model objects
   def index
     @blog_posts = BlogPost::all #@blog_posts is an instance that passes to the view
   end
 
+  # Get items with an specific id
   def show 
     @blog_post = BlogPost::find(params[:id]) 
     rescue 
       redirect_to root_path
   end
 
+  # Show new items form
   def new  
     @blog_post = BlogPost::new
   end
 
+  # Create new model object from the `form`
   def create
      @blog_post = BlogPost.new(blog_post_params)
      
@@ -22,16 +26,16 @@ class BlogPostsController < ApplicationController
      else
       render :new, status: :unprocessable_entity
      end
-     
-     
-     @blog_post.save
+  end
+
+
+  def edit 
   end
 
 
 
-
   private
-
+  # Use to give security in bulk insertion
   def blog_post_params
     params.require(:blog_post).permit(:title, :body) 
   end
